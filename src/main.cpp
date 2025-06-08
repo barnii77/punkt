9f60e6a056e2c00fe77884d90e2c24e6d87ac98c
barnii77 <barnii77@barnii77.dev>
2025-06-08 03:08:51 +0200

    add a tiny cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "punkt/api/punkt.h"
 
 #include <string>
+#include <iostream>
 #include <fstream>
 
 class FileNotFoundException final : std::exception {
     const std::string m_path;
 
+public:
     [[nodiscard]] const char *what() const noexcept override;
 
-public:
     explicit FileNotFoundException(std::string path);
 };
 
@@ -39,7 +40,7 @@ static std::string readInputFile(const char *path) {
     return out;
 }
 
-int main() {
+static void runTestMode() {
     const char *path = "examples/graphviz_gallery/siblings.dot";
     // path = "examples/demo_inheritance.dot";
     // path = "examples/graphviz_gallery/ninja.dot";
@@ -49,6 +50,41 @@ int main() {
     const std::string test_input = readInputFile(path);
     const auto font_path = "resources/fonts/tinyfont.psf";
     punktRun(test_input.data(), font_path);
+}
+
+static void printHelp() {
+    std::cout << "punkt - A tiny clone of dot from graphviz" << std::endl << std::endl << "Usage:" << std::endl <<
+            "punkt file/path.dot" << std::endl << std::endl << "Additional flags:" << std::endl <<
+            "\t--help\tShow this message" << std::endl << "\t-h\tShow this message";
+}
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        printHelp();
+    } else if (argc == 2) {
+        if (const std::string_view arg = argv[1]; arg == "--run-test-mode") {
+            runTestMode();
+        } else if (arg == "--help" || arg == "-h") {
+            printHelp();
+        } else {
+            const char *path = argv[1];
+            std::string test_input;
+            try {
+                test_input = readInputFile(path);
+            } catch (const FileNotFoundException &e) {
+                std::cerr << "Error: " << e.what();
+                return 1;
+            }
+            try {
+                const auto font_path = "resources/fonts/tinyfont.psf";
+                punktRun(test_input.data(), font_path);
+            } catch (const std::exception &e) {
+                std::cerr << "Error: " << e.what();
+            }
+        }
+    } else {
+        std::cerr << "Takes at most 1 argument: {graph_file_path}";
+    }
 }
 
 ////////////////////// TODOS //////////////////////
@@ -63,19 +99,19 @@ int main() {
 // TODO FINISH handle_link_nodes.cpp
 // TODO ADD SPECIAL CASE HANDLING TO per_rank_ordering assignment for IO port ranks so they get assigned according to
 // the ordering in the parent graph
-    // TODO also, I should actually assign the per-rank ordering vectors in handle_link_nodes.cpp already and should
-    // only modify the initial ordering assignment so it skips ranks where the ordering vectors are already populated.
-    // TODO except the above is really shitty software architecture, I have to rethink how I do info propagation.
-    // this probably requires more refactors.
+// TODO also, I should actually assign the per-rank ordering vectors in handle_link_nodes.cpp already and should
+// only modify the initial ordering assignment so it skips ranks where the ordering vectors are already populated.
+// TODO except the above is really shitty software architecture, I have to rethink how I do info propagation.
+// this probably requires more refactors.
 // TODO I have to actually store which node an edge was originally meant to route to when replacing link nodes with
 // cluster super nodes... i.e. the cluster super node edges must store @to A
-    // TODO figure out how to do this with nested clusters
+// TODO figure out how to do this with nested clusters
 // TODO populate the m_io_port_ranks set
 // TODO I have to do a thing similar to ghost edges where I only place the arrow attrs on the correct edges when I have
 // IO port edges
 // TODO make sure to filter out edges like A -> B inside of clusters when neither A nor B are owned by the cluster
-    // TODO I think I'll just delete them, but this is important. Not deleting them (or moving them) would cause
-    // out of bounds memory accesses due to invalidated ingoing node refs in the super-node insertion
+// TODO I think I'll just delete them, but this is important. Not deleting them (or moving them) would cause
+// out of bounds memory accesses due to invalidated ingoing node refs in the super-node insertion
 
 // TODO I could add record nodes, i.e. allow nodes to have internal splits (splitting lines)
 
@@ -91,11 +127,11 @@ int main() {
 // TODO I have to destroy cluster super-nodes at the end too
 // TODO introduce some sort of concept of IO port ranks that cannot be reordered by x optimization but still moved while
 // everything else can be moved, so I need to add special case handling to x opt and the (currently unused) reordering.
-    // TODO use and populate the m_fixed_ordering_ranks
+// TODO use and populate the m_fixed_ordering_ranks
 // TODO think about in what order I want to do rank assignment because the rank assignment of the parent graph should
 // influence that of the children
-    // TODO I'll create the super-node first, then run parent rank assignment, then I'll insert the IO-ports in the
-    // cluster (top or bottom depending on where the other node is relative to the cluster's super-node)
+// TODO I'll create the super-node first, then run parent rank assignment, then I'll insert the IO-ports in the
+// cluster (top or bottom depending on where the other node is relative to the cluster's super-node)
 // TODO I have to leak out layout info from clusters eventually
 // TODO I have to do the layout pipeline of the parent graph first (until the x optimization, i.e. when the x layout has
 // been determined) and then I know the outer ordering of above and below ranks. Then, I can insert the IO ports in the
@@ -103,11 +139,11 @@ int main() {
 // Then I can run the (full) layout pipeline of the cluster.
 // Except sh*t, I need the x opt result of the inner for the outer x opt because super-node dimensions are needed
 // So here's what I'll do:
-    // I'll run the parent pipeline up until initial layout
-    // I'll run initial layout of the cluster
-    // I'll run parent x opt
-    // I'll finish cluster pipeline
-    // I'll re-run parent x opt and finish parent pipeline
+// I'll run the parent pipeline up until initial layout
+// I'll run initial layout of the cluster
+// I'll run parent x opt
+// I'll finish cluster pipeline
+// I'll re-run parent x opt and finish parent pipeline
 // TODO a cluster should become a single super-node when doing all my layout computations. This means it is literally
 // treated as a single (giant) node with special attributes (@type = "cluster", @code = <string_view into the graph
 // source defining the cluster>) and assigned a single rank. Once we have computed the horizontal orderings, we convert
@@ -131,12 +167,12 @@ int main() {
 // simply hook up those top and bottom ports with cluster-external nodes. The entire idea of rail nodes doesn't play too
 // well with my layout engine and very similar visuals as the visuals rail nodes create can emerge from this top/bottom
 // IO port mechanism
-    // TODO this implies throwing a magic attr onto the IO ports that tells you what inner node it connects to
+// TODO this implies throwing a magic attr onto the IO ports that tells you what inner node it connects to
 
 // TODO the current pipeline only works for simple graphs... I need to rework this most likely in a way that optimizes
 // much more effectively on large graphs...
 // TODO figure out why weird suboptimal edge routing happens with the iterative optimization
-    // TODO also why is there often a rightward trend
+// TODO also why is there often a rightward trend
 // TODO I could add bubble reordering into the mix with the actual x optimization, i.e. interleaving the x opt with
 // bubble reordering steps where it will swap adjacent nodes in the ordering
 // TODO I should probably fix those little weird spots splines get on each ghost node when they don't go straight down
@@ -146,8 +182,8 @@ int main() {
 // TODO re-route edge trajectories so they go into the arrow tail instead
 // TODO I have a massive antialiasing problem
 // TODO adapt the GLRenderer so that I can use it for clusters, i.e. with multiple digraphs at once
-    // TODO though I should probably just write out the glyph quads of the cluster into the glyph quads of the parent
-    // graph right before rendering (after x optimization and stuff)
+// TODO though I should probably just write out the glyph quads of the cluster into the glyph quads of the parent
+// graph right before rendering (after x optimization and stuff)
 
 // TODO handle DPI properly, currently, 1 dot = 1 pixel, but that's simply not true
 // TODO handle font loading manually - write a minimal TTF parser and renderer
